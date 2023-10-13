447b798835c746e514c8ee1139df7bed98be7a0f
AlexJakeman <Alexjakeman4@gmail.com>
2023-10-13 21:15:31 +0100

    added img popups for the article pages

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -15,6 +15,17 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
-    $('.image-link').magnificPopup({type:'image'});
-  });
+$(function () {
+    "use strict";
+    
+    $(".popup img").click(function () {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
+    });
+    
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+    });
+    
+});
